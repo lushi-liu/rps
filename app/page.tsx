ce63475f3adbe38021d8503484fcc4bed2520a7c
Lushi Liu <lushiliu1@gmail.com>
2025-09-10 12:19:19 -0600

    added vs player button to home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,19 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-8 text-gray-800">
         Rock-Paper-Scissors Card Game
       </h1>
-      <Link href="/vs-bot">
-        <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600">
-          Play Vs Bot
-        </button>
-      </Link>
+      <div className="flex flex-row gap-4">
+        {" "}
+        <Link href="/vs-bot">
+          <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600">
+            Play Vs Bot
+          </button>
+        </Link>
+        <Link href="/vs-player">
+          <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600">
+            Play Vs Player
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
